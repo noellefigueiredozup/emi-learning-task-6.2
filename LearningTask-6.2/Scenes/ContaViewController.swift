0b56670b4fe64e1d1fee77cea54ebe4449b20c48
noellefigueiredozup <113380830+noellefigueiredozup@users.noreply.github.com>
2022-11-01 12:15:03 -0300

    modelo configurado e ligado ao ViewController

--- LearningTask-6.2/Scenes/ContaViewController.swift
+++ LearningTask-6.2/Scenes/ContaViewController.swift
@@ -11,8 +11,23 @@ class ContaViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TransacaoCell", for: indexPath) as? TransacaoTableViewCell else {
+            fatalError("Não foi possível obter célula com identificador TransacaoCell para a tabela.")
+        }
+        let transacao = Transacao(tipo: .transferenciaRecebida(.pix), valor: 200, interessado: "Alberto Souza")
+        cell.setup(transacao)
+        return cell
+    }
+    
+    
     
 }
